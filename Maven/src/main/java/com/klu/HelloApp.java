10a989e6de1589587101db717d74fbeb63c03974
saivignesh-26 <saivigneshkathiresan@gmail.com>
2026-02-06 11:10:02 +0530

    Add files via upload

---
+++ Maven/src/main/java/com/klu/HelloApp.java
@@ -0,0 +1,11 @@
+package com.klu;
+
+public class HelloApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Maven Project");
+
+	}
+
+}
