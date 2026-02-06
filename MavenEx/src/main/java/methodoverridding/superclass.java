cfe96dceb68adf117f77d84431a204c93a4f22f4
saivignesh-26 <saivigneshkathiresan@gmail.com>
2026-02-06 11:28:53 +0530

    Add files via upload

---
+++ MavenEx/src/main/java/methodoverridding/superclass.java
@@ -0,0 +1,39 @@
+package methodoverridding;
+
+class Bank {
+    double getInterestRate() {
+        return 0.0;
+    }
+}
+
+// Subclass SBI
+class SBI extends Bank {
+    @Override
+    double getInterestRate() {
+        return 2.5;
+    }
+}
+
+// Subclass HDFC
+class HDFC extends Bank {
+    @Override
+    double getInterestRate() {
+        return 5.0;
+    }
+}
+
+public class superclass{
+
+	public static void main(String[] args) {
+	
+		Bank bank;
+
+        bank = new SBI();
+        System.out.println("Final SBI Rate" + bank.getInterestRate() + "%");
+
+        bank = new HDFC();
+        System.out.println("Final HDFC Rate: " + bank.getInterestRate() + "%");
+
+	}
+
+}
